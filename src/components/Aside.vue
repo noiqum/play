185cf51085db9f94d30f842ecb0c0aafa9603b60
onur <onur.coskun@wunder.is>
2021-06-27 14:49:10 +0300

    chore: aside component base

---
+++ src/components/Aside.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="aside">
+      <ul>
+          <li>
+              <i></i>
+          </li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+      </ul>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
