0941bead7c132ae23aa012666c8baa76b683ff1d
onur <onur.coskun@wunder.is>
2021-06-27 18:52:28 +0300

    chore: tailwindCss install and config

--- src/components/Aside.vue
+++ src/components/Aside.vue
@@ -1,25 +1,20 @@
 <template>
-  <div class="aside">
-      <ul>
-          <li>
-              <i></i>
-          </li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-      </ul>
-
+<div class="flex w-full bg-gray-600">
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
   </div>
 </template>
 
 <script>
 export default {
-
+    name:"Aside"
 }
 </script>
 
-<style lang="sass" scoped>
-
+<style scoped>
+.aside{
+    display:"flex";
+    width: "100%";
+    height: 10vh;
+}
 </style>
