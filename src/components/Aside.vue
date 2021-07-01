f5b44375d746d21cf9588386fe7f6fff7cc14a75
onur <onur.coskun@wunder.is>
2021-07-01 23:02:51 +0300

    feat: svg icon component created

--- src/components/Aside.vue
+++ src/components/Aside.vue
@@ -1,20 +1,63 @@
 <template>
-<div class="flex w-full bg-gray-600">
-      <router-link to="/">Home</router-link>
+<div class="flex w-full bg-space aside">
+      <router-link to="/">
+      <!-- eslint-disable-next-line vue/no-parsing-error -->
+       <AsideIconBase :height="32" :width="32" >
+           <Home/>
+       </AsideIconBase>
+       <span class="icon-name">Home</span>
+      </router-link>
       <router-link to="/about">About</router-link>
   </div>
 </template>
 
 <script>
+import AsideIconBase from "../components/AsideIconBase.vue";
+import Home from "../assets/icon/Home.vue";
 export default {
-    name:"Aside"
+    name:"Aside",
+   components:{
+       AsideIconBase,
+       Home
+   }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+@import '../sass/variables.scss';
 .aside{
-    display:"flex";
-    width: "100%";
-    height: 10vh;
+   background-color:$purple4;
+   position: fixed;
+   top: 0;
+   left: 0;
+   min-height: 100vh;
+   display: flex;
+   width: 5vw;
+   flex-direction: column;
+   color: #006466;
+   justify-content: center;
+   align-items: center;
+   z-index: 10;
+   transition: all 0.4s ease-in-out;
+   overflow: hidden;
+   
+   & > a {
+       display: flex;
+      
+        & .icon-name{
+            display: none;
+        }
+    }
+    &:hover{
+        width: 15vw;
+         & .icon-name{
+            display: flex;
+        }
+    }
+    
+    & > a:hover{
+        color: aquamarine;
+       
+    }
 }
 </style>
---
+++ src/components/AsideIconBase.vue
@@ -0,0 +1,35 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" 
+    :width="width" 
+    :height="height" 
+    :viewBox="viewBox" 
+    :aria-labelledby="iconName"
+    :stroke-linejoin="stroke-linejoin"
+    :stroke-width="stroke-width"
+    :stroke-linecap="stroke-linecap"
+    :fill="fill"
+    :stroke="stroke"
+  >
+    <title :id="iconName" lang="en">{{iconName}}</title>
+    
+      <slot />
+    
+  </svg>
+</template>
+<script>
+export default {
+    name:"AsideIconBase",
+  props: {
+    fill:{type:String,default:"none"},
+    "stroke-linejoin":{type:String,default:"round"} ,
+    viewBox: { type: String, default: '0 0 32 32'},
+    iconName: { type: String, default: ''},
+    width: { type: [Number, String], default: 18 },
+    height: {type: [Number, String], default: 18 },
+    iconColor: {type: String, default: 'currentColor'},
+    "stroke-width":{type:String,default:"1"},
+    "stroke-linecap":{type:String,default:"round"},
+    stroke:{type:String,default:"currentColor"}
+  }
+}
+</script>
